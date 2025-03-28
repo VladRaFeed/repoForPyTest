71ae30cb03df2e4620fcc8b11db523c82d4e4761
VladRaFeed <jack22toplolkek@gmail.com>
2025-03-28 10:25:28 +0200

    Lesson 4 complete Parametrize

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,11 @@
-from src.main import A
+import pytest
 
-def test_main():
-    assert A.x == 1
+from src.main import Calculator
 
-def test_2():
-    assert 2 == 2
+@pytest.mark.parametrize("x,y,res", [(1,2,0.5), (5,-1,-5),(4,2,2)])
+def test_divide(x,y,res):
+    assert Calculator().divide(x, y) == res
+    
+@pytest.mark.parametrize("x,y,res", [(1,3,4), (5,-1,4),(0,0,0)])
+def test_add(x,y,res):
+    assert Calculator().add(x,y) == res
