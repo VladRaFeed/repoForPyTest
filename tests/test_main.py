cc8ac915995eafd4e27ef7827a19c5134cfbbd14
VladRaFeed <jack22toplolkek@gmail.com>
2025-03-28 11:24:21 +0200

    Complete lesson 5

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,14 @@
-from src.main import A
+import pytest
+from contextlib import nullcontext as does_not_raise
+from src.main import Calculator
 
-def test_main():
-    assert A.x == 1
-
-def test_2():
-    assert 2 == 2
+class TestCalculator:
+    @pytest.mark.parametrize("x,y,res, expentation", [(1,2,0.5,does_not_raise()), (5,"-1",-5, pytest.raises(TypeError)),(4,0,2,pytest.raises(ZeroDivisionError))])
+    def test_divide(self,x,y,res, expentation):
+        with expentation:
+            assert Calculator().divide(x, y) == res
+        
+    @pytest.mark.parametrize("x,y,res,expentation", [(1,3,4,does_not_raise()), (5,"-1",4,pytest.raises(TypeError)),(0,0,0,does_not_raise())])
+    def test_add(self,x,y,res,expentation):
+        with expentation:
+            assert Calculator().add(x,y) == res
