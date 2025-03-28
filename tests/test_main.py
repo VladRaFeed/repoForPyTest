3ad0496f20d86ddfd1bc8141e37b941835310053
VladRaFeed <jack22toplolkek@gmail.com>
2025-03-28 12:05:15 +0200

    fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,7 +16,7 @@ def test_subtract():
 def test_multiply():
     calculator = Calculator()
     assert calculator.multiply(3, 4) == 12
-    assert calculator.multiply(0, 5) == 5
+    assert calculator.multiply(0, 5) == 0
 
 def test_divide():
     calculator = Calculator()
